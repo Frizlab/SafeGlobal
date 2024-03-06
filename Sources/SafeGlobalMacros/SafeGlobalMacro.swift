b965175410d4fb0c034992f6677a5ece7942af11
Frizlab <fload@me.com>
2024-03-06 20:18:16 +0100

    Remove an obsolete comment

--- Sources/SafeGlobalMacros/SafeGlobalMacro.swift
+++ Sources/SafeGlobalMacros/SafeGlobalMacro.swift
@@ -8,16 +8,6 @@ import SwiftSyntaxMacros
 
 
 
-/**
- Implementation of the `stringify` macro, which takes an expression of any type and produces a tuple containing the value of that expression and the source code that produced the value.
- 
- For example
- 
-     #stringify(x + y)
- 
- will expand to
- 
-     (x + y, "x + y") */
 public struct SafeGlobalMacro : PeerMacro, AccessorMacro {
 	
 	struct NotImplemented : Error {}
