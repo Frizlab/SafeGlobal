36c68156ed0968c8f95a5cb11152f794bc030ad2
Frizlab <fload@me.com>
2024-12-10 16:34:25 +0100

    Remove a test in the client which is not allowed in Swift 6

--- Sources/SafeGlobalClient/main.swift
+++ Sources/SafeGlobalClient/main.swift
@@ -6,7 +6,8 @@ import SafeGlobal
 
 enum Conf : Sendable {
 	
-	@SafeGlobal static let dummy1: Int = 42
+	/* Not allowed in Swift 6. */
+	//@SafeGlobal static let dummy1: Int = 42
 	@SafeGlobal static var dummy2: Int = 42
 	@SafeGlobal static var dummy3: Int!
 	@SafeGlobal static var dummy4: Int?
